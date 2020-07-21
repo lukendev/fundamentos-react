e0200e0bf9a1ef0d0478f9e9c2499aea077be406
lukendev <lucas.azevedo2104@gmail.com>
2020-07-21 17:19:03 -0300

    feat: component props

--- src/index.js
+++ src/index.js
@@ -3,11 +3,16 @@ import ReactDOM from 'react-dom'
 import React from 'react'
 
 import Primeiro from './components/basicos/Primeiro'
+import ComParametro from './components/basicos/ComParametro'
 
 
 ReactDOM.render(
-    <div>
+    <div id='app'>
         <Primeiro></Primeiro>
+        <ComParametro 
+        titulo='Situação do Aluno' 
+        aluno="João" 
+        nota={9.3} />
     </div>,
     document.getElementById('root')
  )
