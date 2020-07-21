6d129f2e79f6a6bae48c70439536aa45d8d7c705
lukendev <lucas.azevedo2104@gmail.com>
2020-07-21 17:05:53 -0300

    feat: JSX into component

--- src/components/basicos/Primeiro.js
+++ src/components/basicos/Primeiro.js
@@ -1,3 +1,11 @@
+import React from 'react'
+
 export default function Primeiro(){
-    return 'Primeiro componente'
+    const msg = 'Seja bem-vindo(a)'
+    return (
+        <div>
+            <h2>Primeiro componente</h2>
+            <p>{ msg }</p>
+        </div>
+    )
 }
