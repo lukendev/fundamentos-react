6e22eeb391a7e548b4c9587fba59a63fab2b2dd3
lukendev <lucas.azevedo2104@gmail.com>
2020-07-21 18:05:15 -0300

    challenge: display random number / feat: destruction

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 
+import Aleatorio from './components/basicos/Aleatorio'
 import Primeiro from './components/basicos/Primeiro'
 import ComParametro from './components/basicos/ComParametro'
 import Fragmento from './components/basicos/Fragmento'
@@ -8,6 +9,7 @@ export default _ =>
 
         <div id='app'>
             <h1>Fundamentos React</h1>
+            <Aleatorio min={1} max={60} />
             <Fragmento />
             <ComParametro 
             titulo='Situação do Aluno' 
