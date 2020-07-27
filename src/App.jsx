5f86a1f25fee96069fb2dcd27101d18a8d2734b8
lukendev <lucas.azevedo2104@gmail.com>
2020-07-27 18:04:49 -0300

    feat: Props for children

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import './App.css'
 import React from 'react'
 
 import Card from './components/layout/Card'
+
+import Familia from './components/basicos/Familia'
 import Aleatorio from './components/basicos/Aleatorio'
 import Primeiro from './components/basicos/Primeiro'
 import ComParametro from './components/basicos/ComParametro'
@@ -13,6 +15,10 @@ export default _ =>
         <h1>Fundamentos React</h1>
 
         <div className="Cards">
+
+            <Card titulo="#05 - Componente com Filhos" color="#00C8F8">
+                <Familia sobrenome="Ferreira" />
+            </Card>
 
             <Card titulo="#04 - Desafio Aleatório" color="#FA6900">
                 <Aleatorio min={1} max={60} />
