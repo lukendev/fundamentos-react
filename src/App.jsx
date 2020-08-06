60cde81e6a725c92434b0f0693576741f2209ff6
lukendev <lucas.azevedo2104@gmail.com>
2020-08-06 15:19:34 -0300

    feat: Loop

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 
 import Card from './components/layout/Card'
 
+import ListaAlunos from './components/repeticao/ListaAlunos'
 import Familia from './components/basicos/Familia'
 import FamiliaMembro from './components/basicos/FamiliaMembro'
 import Aleatorio from './components/basicos/Aleatorio'
@@ -17,8 +18,12 @@ export default _ =>
 
         <div className="Cards">
 
+            <Card titulo="#06 - Repetição" color="#FF4C65">
+                <ListaAlunos></ListaAlunos>
+            </Card>
+
             <Card titulo="#05 - Componente com Filhos" color="#00C8F8">
-                <Familia sobrenome="Ferreira">
+                <Familia sobrenome="Benedito">
                     <FamiliaMembro nome="Pedro" />
                     <FamiliaMembro nome="Ana" />
                     <FamiliaMembro nome="Gustavo" />
---
+++ src/data/alunos.js
@@ -0,0 +1,11 @@
+export default [
+    {id: 1, nome: 'Ana', nota: 9.2},
+    {id: 2, nome: 'Bia', nota: 7.6},
+    {id: 3, nome: 'Carlos', nota: 8.1},
+    {id: 4, nome: 'Daniel', nota: 5.2},
+    {id: 5, nome: 'Gui', nota: 4.5},
+    {id: 6, nome: 'Rebeca', nota: 10.0},
+    {id: 7, nome: 'Arthur', nota: 8.6},
+    {id: 8, nome: 'Pedro', nota: 4.2},
+    {id: 9, nome: 'Gustavo', nota: 6.2}
+]
