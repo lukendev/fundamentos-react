3bcdc5d2b63235a7ff6592c6c8d1f5c2291f30e4
lukendev <lucas.azevedo2104@gmail.com>
2020-08-15 12:36:36 -0300

    fix: Mega Challenge correction

---
+++ src/components/mega/GerarNumeros.js
@@ -0,0 +1,20 @@
+function gerarNumeroNaoContido(min, max, array){
+    const aleatorio = parseInt(Math.random() * (max + 1 - min)) + min
+    return array.includes(aleatorio) ? gerarNumeroNaoContido(min, max, array) : aleatorio
+}
+
+function gerarNumeros(qtd){
+    const numeros = Array(qtd)
+    .fill(0)
+    .reduce((nums) => {
+        const novoNumero = gerarNumeroNaoContido(1, 60, nums)
+        return [ ...nums, novoNumero]
+    }, [])
+    .sort((n1, n2) => n1 - n2)
+
+    return numeros
+}
+
+console.log(gerarNumeros(7))
+
+// console.log(gerarNumeroNaoContido(1, 5, [0, 0, 0, 0, 0, 0]))
